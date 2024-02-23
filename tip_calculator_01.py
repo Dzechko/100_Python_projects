0df7f37d8b836baa4ddc10c52ec4fd9cc51ffab2
Dzechko <dzechko4@gmail.com>
2024-02-23 16:47:12 +0530

    fixed some errors

--- tip_calculator_01.py
+++ tip_calculator_01.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip Calculator")
-x = int(input("What was the total bill: $"))
-t = int(input("What percentage tip would you like to give: $"))
+bill = float(input("What was the total bill: $"))
+tip = int(input("What percentage tip would you like to give: $"))
 n = int(input("How many peoples to split the bill: "))
-a = (x - x*(t/100))/n
+a = (bill + bill*(tip/100))/n
 print(f"Each person should pay {a}")
